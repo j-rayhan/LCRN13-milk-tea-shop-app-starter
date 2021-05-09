ebef98d810ba03d4e389ce0b9ac3c28d74b31ccb
johir-rayhan <johir.rayhan01@gmail.com>
2021-05-09 08:45:32 +0600

    Home Screen Done

--- stores/index.js
+++ stores/index.js
@@ -3,20 +3,6 @@ import themeSlice from './themeSlice';
 
 const store = configureStore({
   reducer: themeSlice.reducer
-})
-
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from 'redux-thunk';
-
-// import themeReducer from './themeReducer'
-
-// let composeEnhancers = compose;
-// if (__DEV__){
-//   composeEnhancers = window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-// }
-// const store = createStore(
-//   themeReducer,
-//   composeEnhancers(applyMiddleware(thunk)),
-// );
+});
 
 export default store;
